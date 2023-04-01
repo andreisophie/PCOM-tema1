a1dff08e496c6bd949984388d830a3e5a1e2336a
andreisophie <andreisophie@yahoo.com>
2023-04-01 13:02:27 +0300

    Implemented IPv4

--- router.c
+++ router.c
@@ -2,8 +2,45 @@
 #include "lib.h"
 #include "protocols.h"
 #include <arpa/inet.h>
+#include <string.h>
 
 #define MAX_RT_SIZE 100000
+#define MAX_ARP_SIZE 100000
+
+// some global variables
+static struct route_table_entry *rtable;
+static int rtable_len;
+static struct arp_entry *arp_table;
+static int arp_table_len;
+
+// comparator sortez descrescator dupa prefix si masca
+int comparator(const void *route1, const void *route2) {
+	struct route_table_entry *rentry1 = (struct route_table_entry *)route1;
+	struct route_table_entry *rentry2 = (struct route_table_entry *)route2;
+
+	if (rentry1->prefix != rentry2->prefix) {
+		return rentry2->prefix - rentry1->prefix;
+	}
+
+	return rentry2->mask - rentry1->mask;
+}
+
+/*
+ Returns a pointer (eg. &rtable[i]) to the best matching route, or NULL if there
+ is no matching route.
+*/
+struct route_table_entry *get_best_route(uint32_t ip_dest) {
+	// TODO: implement binary search for this function
+	/* We can iterate through rtable for (int i = 0; i < rtable_len; i++). Entries in
+	 * the rtable are in network order already */
+	for (int i = 0; i < rtable_len; i++) {
+		/* Cum tabela este sortată, primul match este prefixul ce mai specific */
+		if (rtable[i].prefix == (ip_dest & rtable[i].mask)) {
+			return &rtable[i];
+		}
+	}
+	return NULL;
+}
 
 int main(int argc, char *argv[])
 {
@@ -13,9 +50,13 @@ int main(int argc, char *argv[])
 	// Do not modify this line
 	init(argc - 2, argv + 2);
 
-	// parse route table
-	struct route_table_entry *route_table = calloc(MAX_RT_SIZE, sizeof(struct route_table_entry));
-	read_rtable(argv[1], route_table);
+	// parse route table and sort it
+	rtable = calloc(MAX_RT_SIZE, sizeof(struct route_table_entry));
+	rtable_len = read_rtable(argv[1], rtable);
+	qsort((void *)rtable, rtable_len, sizeof(struct route_table_entry), comparator);
+	// use static arp for destination mac address
+	arp_table = calloc(MAX_ARP_SIZE, sizeof(struct arp_entry));
+	arp_table_len = parse_arp_table("arp_table.txt", arp_table);
 
 	while (1) {
 
@@ -41,13 +82,11 @@ int main(int argc, char *argv[])
 			// verific daca pachetul este pentru mine
 			if (inet_addr(get_interface_ip(interface)) ==  header_ip->daddr) {
 				printf("Acest pachet este pentru mine\n");
+				// TODO: fa ceva
 			} else {
-				printf("Acest pachet este pentru altcineva\n");
 				// verific checksum-ul
 				uint16_t old_check = ntohs(header_ip->check);
 				header_ip->check = 0;
-				printf("checksum pachet = %d\n", old_check);
-				printf("checksum calculat = %d\n", checksum((uint16_t *)header_ip, sizeof(struct iphdr)));
 				if (old_check != checksum((uint16_t *)header_ip, sizeof(struct iphdr))) {
 					printf("checksum gresit\n");
 					continue;
@@ -56,15 +95,27 @@ int main(int argc, char *argv[])
 				// verific TTL
 				if (header_ip->ttl == 1 || header_ip->ttl == 0) {
 					printf("pachetul are ttl<1, trebuie aruncat\n");
-					// trimit raspuns ICMP catre sender
+					// TODO: trimit raspuns ICMP catre sender
 					continue;
 				}
 				header_ip->ttl--;
-				// caut in tabela de rutare
-
 				// actualizez checksum
-				header_ip->check = checksum((uint16_t *)header_ip, sizeof(struct iphdr));
-				
+				header_ip->check = htons(checksum((uint16_t *)header_ip, sizeof(struct iphdr)));
+
+				// caut in tabela de rutare
+				struct route_table_entry *entry_next_hop = get_best_route(header_ip->daddr);
+				uint32_t next_hop_ip = entry_next_hop->next_hop;
+
+				get_interface_mac(interface, eth_hdr->ether_shost);
+				DIE(eth_hdr->ether_shost == NULL, "get_interface_mac error");
+				// look for destination max in ARP table
+				for (int i = 0; i < arp_table_len; i++) {
+					if (arp_table[i].ip == next_hop_ip) {
+						memcpy(eth_hdr->ether_dhost, arp_table[i].mac, 6 * sizeof(uint8_t));
+					}
+				}
+
+				send_to_link(entry_next_hop->interface, buf, len);
 			}
 		} else {
 			if (pck_ether_type == 0x0806) {
